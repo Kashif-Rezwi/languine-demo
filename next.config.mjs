dd7d26e6a04889596c70b4d6d7d0f306d4d2258b
Kashif Rezwi <kashif@swipepages.com>
2025-03-19 08:29:47 +0530

    Add i18n configuration to Next.js setup
    - Defined internationalization settings in next.config.mjs, including supported locales (en, fr, ja, bn) and default locale (en).
    - Disabled automatic locale detection for more control over language selection.

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,12 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    i18n: {
+        locales: ['en', 'fr', 'ja', 'bn'],
+        defaultLocale: 'en',
+        localeDetection: false,
+    },
+};
 
 export default withNextIntl(nextConfig);
