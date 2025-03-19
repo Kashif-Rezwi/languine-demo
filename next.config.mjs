2f7199f4db6403baffea24a441a916de43fe3e71
Kashif Rezwi <kashif@swipepages.com>
2025-03-19 08:37:01 +0530

    Remove i18n configuration from Next.js setup in next.config.mjs, simplifying the configuration for future adjustments.

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,6 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    i18n: {
-        locales: ['en', 'fr', 'ja', 'bn'],
-        defaultLocale: 'en',
-        localeDetection: false,
-    },
-};
+const nextConfig = {};
 
 export default withNextIntl(nextConfig);
